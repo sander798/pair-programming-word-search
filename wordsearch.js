4b3c51d763d5b02351ebf59e2e30c03149a80791
Alexander Evans <sander7989@gmail.com>
2023-07-11 20:40:58 -0600

    add vertical word search

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,19 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    //Go through each column of the letters
+    for (let x = 0; x < letters[0].length; x++) {
+        
+        let column = "";
+        
+        for (let y = 0; y < letters[x].length; y++) {
+            column += letters[y][x];
+        }
+
+        if (column.includes(word)) return true
+    }
+    return false;
 }
 
 module.exports = wordSearch
